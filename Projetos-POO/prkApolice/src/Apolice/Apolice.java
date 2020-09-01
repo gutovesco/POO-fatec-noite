5e78fb1c6b2bf79842453da23625c705e5ba1eb7
gutovesco <guto.vesco@hotmail.com>
2020-09-01 19:26:59 -0300

    Added missing methods

--- Projetos-POO/prkApolice/src/Apolice/Apolice.java
+++ Projetos-POO/prkApolice/src/Apolice/Apolice.java
@@ -11,6 +11,31 @@ public class Apolice {
         this.valorPremio = valorPremio;
     }
 
+    public String getNomeSegurado() {
+        return nomeSegurado;
+    }
+
+    public void setNomeSegurado(String nomeSegurado){
+        this.nomeSegurado = nomeSegurado;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public float getValorPremio(){
+        return valorPremio;
+    }
+
+    public void setValorPremio(float valorPremio){
+        this.valorPremio = valorPremio;
+    }
+
+
     public void imprimir(){
         System.out.println("nomeSegurado = " + nomeSegurado);
         System.out.println("idade = " + idade);
